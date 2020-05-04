d73a9b588b1cdff7c471d2c46c8e03e53bc24b3c
Amal Abdu <abduamal@ymail.com>
2020-05-04 01:38:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,7 @@ def create_project_hash
   # This just opens a file and reads it into a variable
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  # projects: kickstarter.css("li.project.grid_4")
 
   binding.pry
 end
